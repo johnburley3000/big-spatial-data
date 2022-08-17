0c97263bd7499920931ef05e00a45d2582adf9a8
johnburley3000 <john.burley3000@gmail.com>
2022-08-17 10:04:35 -0400

    added comments

--- create_forestcovermap.sh
+++ create_forestcovermap.sh
@@ -2,14 +2,14 @@
 #SBATCH -J FC_map
 #SBATCH -t 6:00:00
 #SBATCH -c 1
-#SBATCH --mem=128g
+#SBATCH --mem=40g
 #SBATCH -o Logs/fc_map-%j.out
 #SBATCH -e Logs/fc_map-%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=john_burley@brown.edu
 
-# Shouldn't take more than 1.5 hrs from testing. 
-# May use an absurd amount of memory for unknown reasons.
+# Shouldn't take more than 1.5 hrs per tile. 
+# Should run with ~ 25-30 GB for most tiles. 
 
 cd /users/jburley/scratch/YTDscratch
 LAB=/users/jburley/data/jburley/TreeDetectionTracking/Amazon2016flowering
@@ -21,7 +21,9 @@ LAB=/users/jburley/data/jburley/TreeDetectionTracking/Amazon2016flowering
 # For descriptions of input:
 # Rscript RCode/create_forestcovermap.R --help
 
-source /gpfs/home/jburley/ccv_modules
+source /gpfs/home/jburley/ccv_modules 
+# equiv.:
+# module load  R/4.0.0 udunits/2.2.24 gcc/8.3 pcre2/10.35 geos/3.8.1 gdal/3.0.4 proj/7.0.0
 
 Rscript ${LAB}/Code/create_forestcovermap.R \
 -t $1 \
@@ -29,3 +31,5 @@ Rscript ${LAB}/Code/create_forestcovermap.R \
 -y $3 \
 -i $4 \
 -o $5
+
+# If merging rasters is desired after processing, probably best to use merge_gdal.py (https://gdal.org/programs/gdal_merge.html)
